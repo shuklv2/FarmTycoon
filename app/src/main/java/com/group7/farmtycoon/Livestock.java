d917ff19c1aee828ac6fcdde731a81b8a5e2fd48
Vatsal Shukla <shuklv2@mcmaster.ca>
2017-03-29 23:45:45 -0400

    Addition of Livestock abstract class

---
+++ app/src/main/java/com/group7/farmtycoon/Livestock.java
@@ -0,0 +1,52 @@
+package com.group7.farmtycoon;
+
+/**
+ * Created by vatsalshukla on 2017-03-29.
+ */
+public abstract class Livestock {
+
+    private boolean state;
+    private int quantity = 0;
+    private int hunger;
+    private int resources;
+
+    public Livestock(){
+        this.state = true;
+        this.quantity++;
+        this.hunger = 100;
+        this.resources = 0;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getResources() {
+        return resources;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setResources(int resources) {
+        this.resources = resources;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+}
