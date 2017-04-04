07dafe9cd0614cbe5554bf204305f983ebbfc151
Jerry Xie <xiez27@mcmaster.ca>
2017-04-04 15:38:37 -0400

    Update weathermenu and weather manager

--- app/src/main/java/com/group7/farmtycoon/WeatherMenu.java
+++ app/src/main/java/com/group7/farmtycoon/WeatherMenu.java
@@ -14,16 +14,16 @@ public class WeatherMenu extends AppCompatActivity {
     }
 
     public void droughtPress(View v){
-        w.setWeatherState(WeatherManager.Drought);
+        w.setWeatherState(WeatherManager.list.Drought);
     }
     public void rainPress(View v){
-        w.setWeatherState(WeatherManager.Rain);
+        w.setWeatherState(WeatherManager.list.Rain);
     }
     public void tornadoPress(View v){
-        w.setWeatherState(WeatherManager.Tornado);
+        w.setWeatherState(WeatherManager.list.Tornado);
     }
     public void sunnyPress(View v){
-        w.setWeatherState(WeatherManager.Sunny);
+        w.setWeatherState(WeatherManager.list.Sunny);
     }
 
     WeatherManager w = new WeatherManager();
