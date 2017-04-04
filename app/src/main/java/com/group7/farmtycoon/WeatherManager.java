6e3d8fc1356728ed6977c4137708256e0d440ce9
Jerry Xie <xiez27@mcmaster.ca>
2017-04-03 20:54:33 -0400

    Update sunny
    sunny updated

--- app/src/main/java/com/group7/farmtycoon/WeatherManager.java
+++ app/src/main/java/com/group7/farmtycoon/WeatherManager.java
@@ -9,40 +9,45 @@ public class WeatherManager {
     private Tornado tornado;
     private Rain rain;
     private Drought drought;
+    private Sunny sunny;
 
 
     public WeatherManager(){
         this.tornado = new Tornado();
         this.rain = new Rain();
         this.drought = new Drought();
+        this.sunny = new Sunny();
     }
 
     public int getWeatherDuration(Weather weather){
         return weather.getDuration();
     }
 
-    public boolean getWeatherState(Weather weather){
-        return weather.getState();
-    }
 
     public void setWeatherDuration(Weather weather, int duration){
         weather.setDuration(duration);
     }
 
-    public void setWeatherState(Weather weather, boolean state){
-        weather.setState(state);
+    public void setWeatherState(list weather){
+        currentWeather = weather;
     }
 
     public void update(){
-        /*if (weather.setState(this.tornado)) {
-            System.out.println("It is tornado.");
+        switch(currentWeather){
+            case Tornado: tornado.update(); break;
+            case Rain: rain.update(); break;
+            case Drought : drought.update(); break;
+            case Sunny: sunny.update(); break;
+
         }
-        else if (weather.setState(this.rain)){
-            System.out.println("It is rain.");
-        }
-        else if (weather.setState(this.drought)){
-            System.out.println("It is drought.");
-        }*/
+
+    public enum list {
+        Tornado, Rain, Drought, Sunny
     }
+
+    private list currentWeather;
+    public list getWeatherState(){ return currentWeather; }
+
+
 
 }
