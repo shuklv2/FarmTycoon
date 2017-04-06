ea2a9f41c4669026e5404120cd8e781f5da55b54
Jerry Xie <xiez27@mcmaster.ca>
2017-04-06 11:50:16 -0400

    Add weather log message

--- app/src/main/java/com/group7/farmtycoon/WeatherMenu.java
+++ app/src/main/java/com/group7/farmtycoon/WeatherMenu.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class WeatherMenu extends AppCompatActivity {
 
@@ -17,16 +19,26 @@ public class WeatherMenu extends AppCompatActivity {
 
     public void droughtPress(View v){
         w.setWeatherState(WeatherManager.list.Drought);
+        Toast.makeText(this, "Drought has been picked!", Toast.LENGTH_LONG).show();
+        GameState.updateLog.add("The weather is drought!");
     }
 
     public void rainPress(View v){
         w.setWeatherState(WeatherManager.list.Rain);
+        Toast.makeText(this, "Rain has been picked!", Toast.LENGTH_LONG).show();
+        GameState.updateLog.add("The weather is rain!");
     }
+
     public void tornadoPress(View v){
         w.setWeatherState(WeatherManager.list.Tornado);
+        Toast.makeText(this, "Tornado has been picked!", Toast.LENGTH_LONG).show();
+        GameState.updateLog.add("The weather is tornado!");
     }
+
     public void sunnyPress(View v){
         w.setWeatherState(WeatherManager.list.Sunny);
+        Toast.makeText(this, "Sunny has been picked!", Toast.LENGTH_LONG).show();
+        GameState.updateLog.add("The weather is sunny!");
     }
 
     WeatherManager w = new WeatherManager();
